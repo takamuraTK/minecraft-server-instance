e64c735e117d4f86beea6b8909cc1cbcc6379b09
takamuraTK <tirpitz.zzz@outlook.jp>
2020-07-25 18:01:15 +0900

    fixed ami

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "main" {
-  ami = data.aws_ssm_parameter.amzn2_ami.value
+  ami = "ami-06ad9296e6cf1e3cf"
   instance_type = "t2.medium"
   vpc_security_group_ids      = [aws_security_group.public.id]
   subnet_id                   = aws_subnet.public.id
@@ -7,10 +7,6 @@ resource "aws_instance" "main" {
   user_data                   = data.template_file.userdata.rendered
   key_name                    = "minecraft"
   tags = { Name = "minecraft" }
-}
-
-data aws_ssm_parameter amzn2_ami {
-  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
 
 resource "aws_eip" "main" {

4e1dd5cf7af0f22aed6e7b43513af736bc28d3d1
takamuraTK <tirpitz.zzz@outlook.jp>
2020-07-25 18:01:48 +0900

    add dynmap ingress

--- vpc.tf
+++ vpc.tf
@@ -37,6 +37,12 @@ resource "aws_security_group" "public" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
+    from_port   = 8123
+    to_port     = 8123
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"

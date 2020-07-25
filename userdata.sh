eb2e17668b3f8e3f40d168e5939e8b766ea8e31b
takamuraTK <tirpitz.zzz@outlook.jp>
2020-07-25 18:00:14 +0900

    set userdata

--- userdata.sh
+++ userdata.sh
@@ -1,13 +1,18 @@
-sudo su
+#!/bin/bash
 
 # install java
-yum update -y
-yum install -y java-1.8.0-openjdk.x86_64
+yum update -y && yum install -y java-1.8.0-openjdk.x86_64
 
 # install minecraft server
-mkdir /etc/minecraft && cd /etc/minecraft
-wget https://cdn.getbukkit.org/spigot/spigot-1.12.2.jar
-java -jar spigot-1.12.2.jar
-sed -e "s/eula=false/eula=true/" /etc/minecraft/eula.txt >/tmp/eula.txt
-mv -f /tmp/eula.txt /etc/minecraft/eula.txt
-java -jar spigot-1.12.2.jar
+mkdir /root/minecraft && cd /root/minecraft
+wget https://cdn.getbukkit.org/spigot/spigot-1.14.4.jar && java -jar spigot-1.14.4.jar
+sed -e "s/eula=false/eula=true/" /root/minecraft/eula.txt >/tmp/eula.txt
+mv -f /tmp/eula.txt /root/minecraft/eula.txt
+
+# add plugin
+wget -P /root/minecraft/plugins https://media.forgecdn.net/files/2990/815/Dynmap-3.1-beta-1-spigot.jar
+wget -P /root/minecraft/plugins https://lol.scarsz.me/WTtrsN/DiscordSRV-Build-1.19.1.jar
+
+# start server
+screen -d -m -S "spigot" java -Xms3G -Xmx4G -XX:+UseConcMarkSweepGC -jar spigot-1.14.4.jar
+# java -jar spigot-1.14.4.jar
